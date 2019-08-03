ec79135d41cbfa17a71c7ce848e785c0411562e3
hero <766653466@qq.com>
2019-08-03 14:37:08 +0800

    '增加mysql执行性能分析配置'

--- src/main/java/com/jfeat/am/module/product/config/MybatisConfig.java
+++ src/main/java/com/jfeat/am/module/product/config/MybatisConfig.java
@@ -1,7 +1,10 @@
 package com.jfeat.am.module.product.config;
 
+import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @MapperScan(
@@ -10,4 +13,13 @@ import org.springframework.context.annotation.Configuration;
 public class MybatisConfig {
     public MybatisConfig() {
     }
+    /**
+     * SQL执行效率插件
+     */
+    @Bean
+    @Profile({"dev","test"})// 设置 dev test 环境开启
+    public PerformanceInterceptor performanceInterceptor() {
+        return new PerformanceInterceptor();
+    }
+
 }
