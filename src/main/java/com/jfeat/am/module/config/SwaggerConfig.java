10fe024a3fbcb222cf43b245e73a173195f71e78
hero <766653466@qq.com>
2019-08-12 11:41:54 +0800

    'swagger访问增加token参数'

--- src/main/java/com/jfeat/am/module/config/SwaggerConfig.java
+++ src/main/java/com/jfeat/am/module/config/SwaggerConfig.java
@@ -3,12 +3,18 @@ package com.jfeat.am.module.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -18,7 +24,18 @@ public class SwaggerConfig {
 
     @Bean
     public Docket helloApi() {
-        return (new Docket(DocumentationType.SWAGGER_2)).apiInfo(this.apiInfo()).select().apis(RequestHandlerSelectors.basePackage("com.jfeat.am.module")).paths(PathSelectors.any()).build();
+
+        List<Parameter> pars = new ArrayList<>();
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        tokenPar.name("Authorization").description("令牌").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+
+        return (new Docket(DocumentationType.SWAGGER_2))
+                .apiInfo(this.apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.jfeat.am.module"))
+                .paths(PathSelectors.any())
+                .build();
     }
 
     private ApiInfo apiInfo() {
