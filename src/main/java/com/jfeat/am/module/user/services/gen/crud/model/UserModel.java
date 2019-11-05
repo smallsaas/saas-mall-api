360bf3b1c562e31a0505488c8f8fb71689b2f90c
vincent@macbook <vincent@macbook.air>
2019-11-05 23:08:59 +0800

    frontUser vs. user

--- src/main/java/com/jfeat/am/module/user/services/gen/crud/model/UserModel.java
+++
@@ -1,9 +0,0 @@
-package com.jfeat.am.module.user.services.gen.crud.model;
-
-import com.jfeat.am.module.user.services.gen.persistence.model.User;
-
-/**
- * Created by Code Generator on 2019-11-05
- */
-public class UserModel extends User{
-}
