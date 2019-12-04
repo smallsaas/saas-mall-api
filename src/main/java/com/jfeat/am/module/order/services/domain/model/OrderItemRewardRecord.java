ffe2347ffbcf7c769ed4cf31798f0bafacbcda45
yejiacheng <yjc@example.com>
2019-12-04 11:50:06 +0800

    增加订单结算的显示字段

--- src/main/java/com/jfeat/am/module/order/services/domain/model/OrderItemRewardRecord.java
+++ src/main/java/com/jfeat/am/module/order/services/domain/model/OrderItemRewardRecord.java
@@ -2,11 +2,65 @@ package com.jfeat.am.module.order.services.domain.model;
 
 import com.jfeat.am.module.order.services.gen.persistence.model.OrderItemReward;
 
+import java.math.BigDecimal;
+
 /**
  * Created by Code Generator on 2019-07-31
  */
 public class OrderItemRewardRecord extends OrderItemReward{
+
+    //json串
+    String proportion;
+    //产品名
+    String productName;
+    //用户名
     String userName;
+    //百分比
+    String percentages;
+    //订单项总价格
+    BigDecimal itemPrice;
+    //订单分红
+    BigDecimal itemReward;
+
+    public BigDecimal getItemReward() {
+        return itemReward;
+    }
+
+    public void setItemReward(BigDecimal itemReward) {
+        this.itemReward = itemReward;
+    }
+
+    public BigDecimal getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(BigDecimal itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public String getPercentages() {
+        return percentages;
+    }
+
+    public void setPercentages(String percentages) {
+        this.percentages = percentages;
+    }
+
+    public String getProportion() {
+        return proportion;
+    }
+
+    public void setProportion(String proportion) {
+        this.proportion = proportion;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
 
     public String getUserName() {
         return userName;
