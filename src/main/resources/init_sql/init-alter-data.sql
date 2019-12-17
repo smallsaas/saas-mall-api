d5fb610cdc5796068a1f17ba9b0f068e5bc09a53
yejiacheng <yjc@example.com>
2019-12-17 18:35:07 +0800

    一些初始化数据 ， 同时 initialize:true-->false

--- src/main/resources/init_sql/init-alter-data.sql
+++ src/main/resources/init_sql/init-alter-data.sql
@@ -17,6 +17,36 @@ alter table  t_express add  org_id bigint(20) default null comment 'org_id';
 alter table  t_ad add  `seq` smallint(6) DEFAULT 0 COMMENT '排序号';
 
 
+alter table  t_config add  org_id bigint(20) default null comment 'org_id';
+insert into t_config_group value("1001"," 商城配置 ",0);
+update t_config set group_id="1001" where key_name = "mall.latest_return_time";
+update t_config set group_id="1001" where key_name = "mall.drawing_conditions";
+update t_config set group_id="1001" where key_name = "mall.auto_validation_receiving_deadline";
+update t_config set group_id="1001" where key_name = "mall.pay_order_timeout";
+update t_config set group_id="1001" where key_name = "mall.auto_offsell";
+update t_config set group_id="1001" where key_name = "mall.order_created_enable";
+update t_config set group_id="1001" where key_name = "mall.auto_deliver_order";
+insert into t_config_group value("1002"," 微信配置 ",0);
+update t_config set group_id="1002" where key_name = "wx.token";
+update t_config set group_id="1002" where key_name = "wx.encrypt_message";
+update t_config set group_id="1002" where key_name = "wx.encoding_aes_key";
+update t_config set group_id="1002" where key_name = "wx.app_id";
+update t_config set group_id="1002" where key_name = "wx.app_secret";
+update t_config set group_id="1002" where key_name = "wx.host";
+update t_config set group_id="1002" where key_name = "wx.partner_id";
+update t_config set group_id="1002" where key_name = "wx.partner_key";
+update t_config set group_id="1002" where key_name = "wx.cert_path";
+update t_config set group_id="1002" where key_name = "wx.wxa_appid";
+update t_config set group_id="1002" where key_name = "wx.auto_reg";
+update t_config set group_id="1002" where key_name = "wx.app_appid";
+update t_config set group_id="1002" where key_name = "wx.app_app_secret";
+update t_config set group_id="1002" where key_name = "appName";
+update t_config set group_id="1002" where key_name = "appPartnerId";
+update t_config set group_id="1002" where key_name = "appPartnerKey";
+ insert into t_config_group value("1003"," 微信物流助手配置 ",0);
+update t_config set group_id="1003" where group_id = "40";
+
+
 
 
 
--- src/main/resources/init_sql/init-schema.sql
+++ src/main/resources/init_sql/init-schema.sql
@@ -274,5 +274,22 @@ CREATE TABLE `ow_page` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 collate utf8mb4_unicode_ci;
 
+
+DROP TABLE IF EXISTS `t_stock_images`;
+CREATE TABLE `t_stock_images` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `url` VARCHAR(255) NOT NULL COMMENT 'images address',
+  `stock_id` BIGINT(20) NOT NULL COMMENT 'owner id',
+  `stock_type` varchar(20) NOT NULL COMMENT 'owner type:like product/member and etc...',
+  `name` varchar(100) DEFAULT NULL COMMENT 'owner type:like product/member and etc...',
+  `image_note` varchar(255) DEFAULT NULL COMMENT 'owner type:like product/member and etc...',
+  `is_primary` smallint(5) default 0 COMMENT 'is primary,0 means not ,1 means primary ',
+  `create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'upload time',
+  UNIQUE(`url`,`stock_id`,`stock_type`),
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+
+
 SET FOREIGN_KEY_CHECKS=1;
 
